414d25cada0a899632e5af5d2cd4570be1c68c6e
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2023-03-27 14:10:36 -0400

    Initial commit

--- fft.py
+++ fft.py
@@ -1,3 +1,31 @@
+import sys
+import numpy
+import matplotlib as plt
 
 
+def default_mode():
+    # Fast Mode where image is converted to its FFT form and displayed
+
+    return None
+
+
+def second_mode():
+    # Denoise an image by applying an FFT
+    return None
+
+def third_mode():
+    # Compressing and saving image
+    return None
+
+def fourth_mode():
+    # Plotting runtime graphs for report
+    return None
+
+
+if __name__ == "__main__":
+    print(f"Arguments count: {len(sys.argv)}")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}:{arg}")
+    arg_list = sys.argv
+    
 
