6acbef1d5a48dd3301f669fd9be37bbb81f78cd8
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2023-03-27 14:24:15 -0400

    Command line

--- fft.py
+++ fft.py
@@ -5,20 +5,23 @@ import matplotlib as plt
 
 def default_mode():
     # Fast Mode where image is converted to its FFT form and displayed
-
+    print("First mode")
     return None
 
 
 def second_mode():
     # Denoise an image by applying an FFT
+    print("Second mode")
     return None
 
 def third_mode():
     # Compressing and saving image
+    print("Third mode")
     return None
 
 def fourth_mode():
     # Plotting runtime graphs for report
+    print("Fourth mode")
     return None
 
 
@@ -26,6 +29,30 @@ if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}:{arg}")
-    arg_list = sys.argv
     
+    mode = "1"
+    image = "moonlanding.png"
+
+    if len(sys.argv) - 1 == 2:
+        mode = sys.argv[1]
+        image = sys.argv[2]
+    elif len(sys.argv) -1 == 1: 
+        mode = sys.argv[1]
+    
+    print(f"mode is {mode}; image is {image}")
+
+
+    if mode == "1":
+        default_mode()
+
+    elif mode == "2":
+        second_mode()
+
+    elif mode == "3":
+        third_mode()
+
+    else:
+        fourth_mode()
+
+
 
